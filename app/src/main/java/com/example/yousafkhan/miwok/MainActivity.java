5dc3a1854d2f0a085cef2468c5ed85df1e8a3c7b
Yousaf Khan <khanyousaf184@gmail.com>
2018-10-24 01:38:37 +0500

    styled main activity, added click listeners to category links, modified manifest file

--- app/src/main/java/com/example/yousafkhan/miwok/MainActivity.java
+++ app/src/main/java/com/example/yousafkhan/miwok/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.yousafkhan.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersActivity(View v) {
+        Intent numbersActivity = new Intent(this, NumbersActivity.class);
+        startActivity(numbersActivity);
+    }
+
+    public void openFamilyActivity(View v) {
+        Intent familyActivity = new Intent(this, FamilyActivity.class);
+        startActivity(familyActivity);
+    }
+
+    public void openColorsActivity(View v) {
+        Intent colorsActivity = new Intent(this, ColorsActivity.class);
+        startActivity(colorsActivity);
+    }
+
+    public void openPhrasesActivity(View v) {
+        Intent phrasesActivity = new Intent(this, PhrasesActivity.class);
+        startActivity(phrasesActivity);
     }
 }
