74b7b6177463781f40f3718ec1f6a44b30f4532b
Yousaf Khan <khanyousaf184@gmail.com>
2018-10-25 01:51:54 +0500

    fixed issue of layout in phrases activity

--- app/src/main/java/com/example/yousafkhan/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/yousafkhan/miwok/PhrasesActivity.java
@@ -13,7 +13,7 @@ public class PhrasesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.activity_phrases);
 
         populatePhrasesList();
         populatePhrasesListview();
